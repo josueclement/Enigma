352f0d6c6d15b00c3a8ceec7d265e056d3fcf38b
Josué Clément <mod6991@gmail.com>
2023-03-23 14:15:44 +0100

    Update SymKey test data

--- Tests/UnitTests/data/SymKey/aes.py
+++ Tests/UnitTests/data/SymKey/aes.py
@@ -1,41 +1,29 @@
+import pandas as pd
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
-from struct import pack, unpack
-from typing import BinaryIO
-
-
-def read_lv(stream: BinaryIO):
-    data_len = stream.read(4)
-    value_len = unpack('i', data_len)[0]
-    return stream.read(value_len)
-
-
-def write_lv(stream: BinaryIO, value: bytes):
-    data_len = pack('i', len(value))
-    stream.write(data_len)
-    stream.write(value)
-
-
-def write_l(stream: BinaryIO, value: int):
-    data_len = pack('i', value)
-    stream.write(data_len)
 
 
 def main():
-    with open('aes.dat', 'wb') as aes_data_dat:
-        write_l(aes_data_dat, 50)
 
-        for i in range(1, 101):
-            key = get_random_bytes(32)
-            write_lv(aes_data_dat, key)
-            iv = get_random_bytes(16)
-            write_lv(aes_data_dat, iv)
-            data = get_random_bytes(i * 16)
-            write_lv(aes_data_dat, data)
+    d = { 'key': [], 'iv': [], 'data': [], 'enc': [] }
 
-            cipher = AES.new(key, AES.MODE_CBC, iv)
-            enc = cipher.encrypt(data)
-            write_lv(aes_data_dat, enc)
+    # Generate random data
+    for i in range(1, 21):
+        key = get_random_bytes(32)
+        iv = get_random_bytes(16)
+        data = get_random_bytes(i * 16)
+        
+        # Encrypt data
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        enc = cipher.encrypt(data)
+
+        d['key'].append(key.hex())
+        d['iv'].append(iv.hex())
+        d['data'].append(data.hex())
+        d['enc'].append(enc.hex())
+    
+    # Save to csv
+    pd.DataFrame(d).to_csv('aes.csv', index=False)
 
 
 if __name__ == '__main__':
