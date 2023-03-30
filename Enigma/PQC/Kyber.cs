255c59b51f3fc645d9806ef5ad7d5daa362852c9
Josué Clément <mod6991@gmail.com>
2023-03-30 14:59:07 +0200

    Add PEM Save/Load for Kyber

--- Enigma/PQC/Kyber.cs
+++ Enigma/PQC/Kyber.cs
@@ -1,10 +1,54 @@
-﻿using Org.BouncyCastle.Crypto;
+﻿using Enigma.IO;
+using Enigma.KDF;
+using Enigma.Padding;
+using Enigma.Random;
+using Enigma.SymKey;
+using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Pqc.Crypto.Crystals.Kyber;
 using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Enigma.PQC
 {
+    internal readonly struct KyberComponentsSizes
+    {
+        public KyberComponentsSizes(int t, int rho, int hpk, int s, int nonce)
+        {
+            T = t;
+            Rho = rho;
+            Hpk = hpk;
+            S = s;
+            Nonce = nonce;
+        }
+
+        public int T { get; }
+        public int Rho { get; }
+        public int Hpk { get; }
+        public int S { get; }
+        public int Nonce { get; }
+
+        public static KyberComponentsSizes GetComponentsSizes(string name)
+        {
+            switch (name)
+            {
+                case "kyber512":
+                case "kyber512-aes":
+                    return new KyberComponentsSizes(768, 32, 32, 768, 32);
+                case "kyber768":
+                case "kyber768-aes":
+                    return new KyberComponentsSizes(1152, 32, 32, 1152, 32);
+                case "kyber1024":
+                case "kyber1024-aes":
+                    return new KyberComponentsSizes(1536, 32, 32, 1536, 32);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+
     /// <summary>
     /// Kyber helper class for key pair generation and sym key generation/extraction
     /// </summary>
@@ -63,7 +107,21 @@ namespace Enigma.PQC
         /// <param name="input">Input stream</param>
         public static KyberPublicKeyParameters LoadPublicKeyFromPEM(Stream input)
         {
-            throw new System.NotImplementedException();
+            PemContent pem = Pem.Read(input);
+            string name = pem.Header.FirstOrDefault(x => x.Name == "Name")?.Value ?? throw new InvalidOperationException();
+            byte[] data = pem.Data ?? throw new InvalidOperationException();
+
+            KyberParameters parameters = GetParameters(name);
+            KyberComponentsSizes sizes = KyberComponentsSizes.GetComponentsSizes(name);
+            byte[] t = new byte[sizes.T];
+            byte[] rho = new byte[sizes.Rho];
+
+            int index = 0;
+            Array.Copy(data, index, t, 0, sizes.T);
+            index += sizes.T;
+            Array.Copy(data, index, rho, 0, sizes.Rho);
+
+            return new KyberPublicKeyParameters(parameters, t, rho);
         }
 
         /// <summary>
@@ -72,7 +130,31 @@ namespace Enigma.PQC
         /// <param name="input">Input stream</param>
         public static KyberPrivateKeyParameters LoadPrivateKeyFromPEM(Stream input)
         {
-            throw new System.NotImplementedException();
+            PemContent pem = Pem.Read(input);
+            string name = pem.Header.FirstOrDefault(x => x.Name == "Name")?.Value ?? throw new InvalidOperationException();
+            byte[] data = pem.Data ?? throw new InvalidOperationException();
+
+            KyberParameters parameters = GetParameters(name);
+            KyberComponentsSizes sizes = KyberComponentsSizes.GetComponentsSizes(name);
+
+            byte[] s = new byte[sizes.S];
+            byte[] t = new byte[sizes.T];
+            byte[] rho = new byte[sizes.Rho];
+            byte[] hpk = new byte[sizes.Hpk];
+            byte[] nonce = new byte[sizes.Nonce];
+
+            int index = 0;
+            Array.Copy(data, index, s, 0, sizes.S);
+            index += sizes.S;
+            Array.Copy(data, index, t, 0, sizes.T);
+            index += sizes.T;
+            Array.Copy(data, index, rho, 0, sizes.Rho);
+            index += sizes.Rho;
+            Array.Copy(data, index, hpk, 0, sizes.Hpk);
+            index += sizes.Hpk;
+            Array.Copy(data, index, nonce, 0, sizes.Nonce);
+
+            return new KyberPrivateKeyParameters(parameters, s, hpk, nonce, t, rho);
         }
 
         /// <summary>
@@ -82,7 +164,39 @@ namespace Enigma.PQC
         /// <param name="password">Password</param>
         public static KyberPrivateKeyParameters LoadPrivateKeyFromPEM(Stream input, string password)
         {
-            throw new System.NotImplementedException();
+            PemContent pem = Pem.Read(input);
+            string name = pem.Header.FirstOrDefault(x => x.Name == "Name")?.Value ?? throw new InvalidOperationException();
+            string saltStr = pem.Header.FirstOrDefault(x => x.Name == "Salt")?.Value ?? throw new InvalidOperationException();
+            string ivStr = pem.Header.FirstOrDefault(x => x.Name == "IV")?.Value ?? throw new InvalidOperationException();
+            byte[] enc = pem.Data ?? throw new InvalidOperationException();
+
+            byte[] key = PBKDF2.GenerateKeyFromPassword(AES.KEY_SIZE, password, Hex.Decode(saltStr), 600_000);
+            byte[] iv = Hex.Decode(ivStr);
+            byte[] dec = AES.DecryptCBC(enc, key, iv);
+
+            byte[] data = new Pkcs7Padding().Unpad(dec, AES.BLOCK_SIZE);
+
+            KyberParameters parameters = GetParameters(name);
+            KyberComponentsSizes sizes = KyberComponentsSizes.GetComponentsSizes(name);
+
+            byte[] s = new byte[sizes.S];
+            byte[] t = new byte[sizes.T];
+            byte[] rho = new byte[sizes.Rho];
+            byte[] hpk = new byte[sizes.Hpk];
+            byte[] nonce = new byte[sizes.Nonce];
+
+            int index = 0;
+            Array.Copy(data, index, s, 0, sizes.S);
+            index += sizes.S;
+            Array.Copy(data, index, t, 0, sizes.T);
+            index += sizes.T;
+            Array.Copy(data, index, rho, 0, sizes.Rho);
+            index += sizes.Rho;
+            Array.Copy(data, index, hpk, 0, sizes.Hpk);
+            index += sizes.Hpk;
+            Array.Copy(data, index, nonce, 0, sizes.Nonce);
+
+            return new KyberPrivateKeyParameters(parameters, s, hpk, nonce, t, rho);
         }
 
         /// <summary>
@@ -92,12 +206,17 @@ namespace Enigma.PQC
         /// <param name="output">Output stream</param>
         public static void SavePublicKeyToPEM(KyberPublicKeyParameters publicKey, Stream output)
         {
-            // !! save publicKey.Parameters infos
-            // - Name : byte (to identify the type: 0x01=kyber512, 0x02=kyber768, etc..)
-            // - K: 2/3/4 as byte too
-            // - sessionKeySize: 128/192/256 as byte too
-            // - usingAes: true/false as byte too
+            List<PemHeaderItem> header = new List<PemHeaderItem>
+            {
+                new PemHeaderItem
+                {
+                    Name = "Name",
+                    Value = publicKey.Parameters.Name.ToString()
+                }
+            };
 
+            byte[] data = publicKey.GetEncoded();
+            Pem.Write("KYBER PUBLIC KEY", header, data, output);
         }
 
         /// <summary>
@@ -107,7 +226,18 @@ namespace Enigma.PQC
         /// <param name="output">Output stream</param>
         public static void SavePrivateKeyToPEM(KyberPrivateKeyParameters privateKey, Stream output)
         {
+            List<PemHeaderItem> header = new List<PemHeaderItem>
+            {
+                new PemHeaderItem
+                {
+                    Name = "Name",
+                    Value = privateKey.Parameters.Name.ToString()
+                }
+            };
 
+            byte[] data = privateKey.GetEncoded();
+            Pem.Write("KYBER PRIVATE KEY", header, data, output);
+            Array.Clear(data, 0, data.Length);
         }
 
         /// <summary>
@@ -118,7 +248,62 @@ namespace Enigma.PQC
         /// <param name="password">Password</param>
         public static void SavePrivateKeyToPEM(KyberPrivateKeyParameters privateKey, Stream output, string password)
         {
-            // !! in addition to the parameters info, write the salt 
+            byte[] salt = RandomHelper.GenerateBytes(16);
+            byte[] iv = RandomHelper.GenerateBytes(AES.IV_SIZE);
+
+            List<PemHeaderItem> header = new List<PemHeaderItem>
+            {
+                new PemHeaderItem
+                {
+                    Name = "Name",
+                    Value = privateKey.Parameters.Name.ToString()
+                },
+                new PemHeaderItem
+                {
+                    Name = "Salt",
+                    Value = Hex.Encode(salt).ToUpper()
+                },
+                new PemHeaderItem
+                {
+                    Name = "IV",
+                    Value = Hex.Encode(iv).ToUpper()
+                }
+            };
+
+            byte[] data = privateKey.GetEncoded();
+            byte[] padded = new Pkcs7Padding().Pad(data, AES.BLOCK_SIZE);
+            byte[] key = PBKDF2.GenerateKeyFromPassword(AES.KEY_SIZE, password, salt, 600_000);
+            byte[] enc = AES.EncryptCBC(padded, key, iv);
+            Array.Clear(key, 0, key.Length);
+            Array.Clear(data, 0, data.Length);
+            Array.Clear(padded, 0, padded.Length);
+
+            Pem.Write("KYBER ENCRYPTED PRIVATE KEY", header, enc, output);
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static KyberParameters GetParameters(string name)
+        {
+            switch (name)
+            {
+                case "kyber512":
+                    return KyberParameters.kyber512;
+                case "kyber512-aes":
+                    return KyberParameters.kyber512_aes;
+                case "kyber768":
+                    return KyberParameters.kyber768;
+                case "kyber768-aes":
+                    return KyberParameters.kyber768_aes;
+                case "kyber1024":
+                    return KyberParameters.kyber1024;
+                case "kyber1024-aes":
+                    return KyberParameters.kyber1024_aes;
+                default:
+                    throw new InvalidOperationException();
+            }
         }
 
         #endregion
