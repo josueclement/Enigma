9e4963758bc604b8b22d8b529232749d7173ec46
Josué Clément <josue.d.clement@gmail.com>
2025-03-24 21:36:28 +0100

    Start implementing RSA service

--- Enigma/PublicKey/RsaService.cs
+++ Enigma/PublicKey/RsaService.cs
@@ -2,30 +2,72 @@ using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Encodings;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Generators;
-using Org.BouncyCastle.Crypto.Modes;
-using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Security;
 
 namespace Enigma.PublicKey;
 
-public class RsaService
+/// <summary>
+/// RSA service
+/// </summary>
+public class RsaService : PublicKeyServiceBase
 {
-    public AsymmetricCipherKeyPair GenerateKeyPair(int keySize = 4096)
+    /// <inheritdoc />
+    protected override IAsymmetricCipherKeyPairGenerator BuildKeyPairGenerator(int keySize)
     {
-        var generator = new RsaKeyPairGenerator(); //IAsymmetricCipherKeyPairGenerator
+        var generator = new RsaKeyPairGenerator();
         generator.Init(new KeyGenerationParameters(new SecureRandom(), keySize));
-        return generator.GenerateKeyPair();
+        return generator;
     }
 
-    // public byte[] Encrypt(byte[] data, AsymmetricKeyParameter publicKey)
+    /// <inheritdoc />
+    protected override IAsymmetricBlockCipher BuildCipher(bool forEncryption, AsymmetricKeyParameter key)
+    {
+        var cipher = new Pkcs1Encoding(new RsaEngine());
+        cipher.Init(forEncryption, key);
+        return cipher;
+    }
+
+    /// <inheritdoc />
+    protected override ISigner BuildSigner(bool forSigning, AsymmetricKeyParameter key)
+    {
+        var signer = SignerUtilities.GetSigner("SHA256withRSA");
+        signer.Init(forSigning, key);
+        return signer;
+    }
+
+    // public void SaveKey(AsymmetricKeyParameter key, Stream output)
     // {
-    //     // var engine = new RsaEngine();
-    //     // engine.Init(true, publicKey);
-    //     // CipherUtilities.GetCipher()
-    //     // engine.pro
-    //     var cipher = new Pkcs1Encoding(new RsaEngine());
-    //     var parameters = new ParametersWithRandom(publicKey, new SecureRandom());
-    //     cipher.Init(true, parameters);
-    //     cipher.ProcessBlock()
+    //     using var writer = new StreamWriter(output, Encoding.UTF8);
+    //     var pemWriter = new PemWriter(writer);
+    //     pemWriter.WriteObject(key);
+    // }
+    //
+    // public void SavePrivateKey(AsymmetricKeyParameter privateKey, Stream output, string password, string algorithm = "AES-256-CBC")
+    // {
+    //     using var writer = new StreamWriter(output, Encoding.UTF8);
+    //     var pemWriter = new PemWriter(writer);
+    //     pemWriter.WriteObject(privateKey, algorithm, password.ToCharArray(), new SecureRandom());
+    // }
+    //
+    // public AsymmetricKeyParameter LoadKey(Stream input)
+    // {
+    //     using var reader = new StreamReader(input, Encoding.UTF8);
+    //     var pemReader = new PemReader(reader);
+    //     object obj = pemReader.ReadObject();
+    //
+    //     if (obj is AsymmetricCipherKeyPair ackp)
+    //     {
+    //         return ackp;
+    //     }
+    //
+    //     if (obj is RsaPrivateCrtKeyParameters rpckp)
+    //     {
+    //         return rpckp;
+    //     }
+    //
+    //     if (obj is RsaKeyParameters rkp)
+    //     {
+    //         return rkp;
+    //     }
     // }
 }
