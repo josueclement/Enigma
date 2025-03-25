83d7a9324baa45025ecbb6f41f0b706e9e136857
Josué Clément <josue.d.clement@gmail.com>
2025-03-25 11:53:00 +0100

    Add NoPaddingService and unit tests

---
+++ Enigma/Padding/NoPaddingService.cs
@@ -0,0 +1,15 @@
+﻿namespace Enigma.Padding;
+
+/// <summary>
+/// No padding service
+/// </summary>
+public class NoPaddingService : IPaddingService
+{
+    /// <inheritdoc />
+    public byte[] Pad(byte[] data, int blockSize)
+        => data;
+
+    /// <inheritdoc />
+    public byte[] Unpad(byte[] data, int blockSize)
+        => data;
+}
---
+++ UnitTests/BlockCiphers/AesCbcServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Enigma.BlockCiphers;
+using Enigma.DataEncoding;
+using Enigma.Padding;
+
+namespace UnitTests.BlockCiphers;
+
+public class AesCbcServiceTests
+{
+    [Theory]
+    [MemberData(nameof(GetCsvValues))]
+    public async Task CsvEncryptTest(byte[] key, byte[] iv, byte[] data, byte[] encrypted)
+    {
+        var srvc = new AesCbcService();
+        using var msInput = new MemoryStream(data);
+        using var msOutput = new MemoryStream();
+
+        await srvc.EncryptAsync(msInput, msOutput, key, iv, new NoPaddingService());
+        Assert.Equal(encrypted, msOutput.ToArray());
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetCsvValues))]
+    public async Task CsvDecryptTest(byte[] key, byte[] iv, byte[] data, byte[] encrypted)
+    {
+        var srvc = new AesCbcService();
+        using var msInput = new MemoryStream(encrypted);
+        using var msOutput = new MemoryStream();
+
+        await srvc.DecryptAsync(msInput, msOutput, key, iv, new NoPaddingService());
+        Assert.Equal(data, msOutput.ToArray()); 
+    }
+    
+    public static IEnumerable<object[]> GetCsvValues()
+    {
+        var hex = new HexService();
+        
+        return File.ReadAllLines(@"BlockCiphers\aes-cbc.csv")
+            .Skip(1)
+            .Select(line =>
+            {
+                var values = line.Split(',');
+                return new object[]
+                {
+                    hex.Decode(values[0]), // key
+                    hex.Decode(values[1]), // iv
+                    hex.Decode(values[2]), // data
+                    hex.Decode(values[3]) // encrypted
+                };
+            });
+    } 
+}
