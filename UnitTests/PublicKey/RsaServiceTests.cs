4d579ddfb84ef376580cbc33829ada570cf8f52f
Josué Clément <josue.d.clement@gmail.com>
2025-03-27 16:06:09 +0100

    Improve public key service

--- UnitTests/PublicKey/RsaServiceTests.cs
+++ UnitTests/PublicKey/RsaServiceTests.cs
@@ -1,5 +1,10 @@
 ﻿using Enigma.Extensions;
 using Enigma.PublicKey;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
 using System.IO;
 using System.Text;
 
@@ -7,13 +12,17 @@ namespace UnitTests.PublicKey;
 
 public class RsaServiceTests
 {
+    IAsymmetricBlockCipher GetCipher() => new Pkcs1Encoding(new RsaEngine());
+    IAsymmetricCipherKeyPairGenerator GetKeyPairGenerator() => new RsaKeyPairGenerator();
+    ISigner GetSigner() => SignerUtilities.GetSigner("SHA256withRSA");
+    
     [Fact]
     public void LoadPublicKey()
     {
-        var srvc = new RsaService();
+        var service = new PublicKeyService(GetCipher, GetKeyPairGenerator, GetSigner);
 
         using var input = new FileStream(@"PublicKey\pub_key1.pem", FileMode.Open, FileAccess.Read);
-        var key = srvc.LoadKey(input);
+        var key = service.LoadKey(input);
 
         Assert.NotNull(key);
     }
@@ -21,10 +30,10 @@ public class RsaServiceTests
     [Fact]
     public void LoadPrivateKey()
     {
-        var srvc = new RsaService();
+        var service = new PublicKeyService(GetCipher, GetKeyPairGenerator, GetSigner);
         
         using var input = new FileStream(@"PublicKey\pk_key1.pem", FileMode.Open, FileAccess.Read);
-        var key = srvc.LoadPrivateKey(input, "test1234");
+        var key = service.LoadPrivateKey(input, "test1234");
         
         Assert.NotNull(key);
     }
@@ -32,33 +41,33 @@ public class RsaServiceTests
     [Fact]
     public void SignVerify()
     {
-        var srvc = new RsaService();
+        var service = new PublicKeyService(GetCipher, GetKeyPairGenerator, GetSigner);
         
         using var inputPrivate = new FileStream(@"PublicKey\pk_key1.pem", FileMode.Open, FileAccess.Read);
-        var privateKey = srvc.LoadPrivateKey(inputPrivate, "test1234");
+        var privateKey = service.LoadPrivateKey(inputPrivate, "test1234");
         using var inputPublic = new FileStream(@"PublicKey\pub_key1.pem", FileMode.Open, FileAccess.Read);
-        var publicKey = srvc.LoadKey(inputPublic);
+        var publicKey = service.LoadKey(inputPublic);
         
         var data = Encoding.UTF8.GetBytes("This message will be signed and verified");
-        var signature = srvc.Sign(data, privateKey);
-        var result = srvc.Verify(data, signature, publicKey);
+        var signature = service.Sign(data, privateKey);
+        var result = service.Verify(data, signature, publicKey);
         Assert.True(result);
     }
 
     [Fact]
     public void SignVerifyBadMessage()
     {
-        var srvc = new RsaService();
+        var service = new PublicKeyService(GetCipher, GetKeyPairGenerator, GetSigner);
         
         using var inputPrivate = new FileStream(@"PublicKey\pk_key1.pem", FileMode.Open, FileAccess.Read);
-        var privateKey = srvc.LoadPrivateKey(inputPrivate, "test1234");
+        var privateKey = service.LoadPrivateKey(inputPrivate, "test1234");
         using var inputPublic = new FileStream(@"PublicKey\pub_key1.pem", FileMode.Open, FileAccess.Read);
-        var publicKey = srvc.LoadKey(inputPublic);
+        var publicKey = service.LoadKey(inputPublic);
         
         var data = Encoding.UTF8.GetBytes("This message will be signed and verified");
         var otherData = Encoding.UTF8.GetBytes("This is not gonna work !");
-        var signature = srvc.Sign(data, privateKey);
-        var result = srvc.Verify(otherData, signature, publicKey);
+        var signature = service.Sign(data, privateKey);
+        var result = service.Verify(otherData, signature, publicKey);
         Assert.False(result);
     }
 }
