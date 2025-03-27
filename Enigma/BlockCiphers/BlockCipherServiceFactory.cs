cf5da7e126793a9b97097f4c1043cead1b57cef0
Josué Clément <josue.d.clement@gmail.com>
2025-03-27 18:07:01 +0100

    Small fix

--- Enigma/BlockCiphers/BlockCipherServiceFactory.cs
+++ Enigma/BlockCiphers/BlockCipherServiceFactory.cs
@@ -13,26 +13,30 @@ public interface IBlockCipherServiceFactory
     /// Create a block cipher service with Electronic Code Book (ECB) mode
     /// </summary>
     /// <param name="engineFactory">Engine factory</param>
-    IBlockCipherService CreateEcbBlockCipherService(Func<IBlockCipher> engineFactory);
+    /// <param name="bufferSize">Buffer size</param>
+    IBlockCipherService CreateEcbBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize);
     
     /// <summary>
     /// Create a block cipher service with Cipher-Block-Chaining (CBC) mode
     /// </summary>
     /// <param name="engineFactory">Engine factory</param>
-    IBlockCipherService CreateCbcBlockCipherService(Func<IBlockCipher> engineFactory);
+    /// <param name="bufferSize">Buffer size</param>
+    IBlockCipherService CreateCbcBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize);
     
     /// <summary>
     /// Create a block cipher service with Segmented Integer Counter (SIC) mode
     /// </summary>
     /// <param name="engineFactory">Engine factory</param>
-    IBlockCipherService CreateSicBlockCipherService(Func<IBlockCipher> engineFactory);
+    /// <param name="bufferSize">Buffer size</param>
+    IBlockCipherService CreateSicBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize);
     
     /// <summary>
     /// Create a block cipher service with Cipher-FeedBack (CFB) mode
     /// </summary>
     /// <param name="engineFactory">Engine factory</param>
     /// <param name="bitBlockSize">Bit block size</param>
-    IBlockCipherService CreateCfbBlockCipherService(Func<IBlockCipher> engineFactory, int bitBlockSize);
+    /// <param name="bufferSize">Buffer size</param>
+    IBlockCipherService CreateCfbBlockCipherService(Func<IBlockCipher> engineFactory, int bitBlockSize, int bufferSize);
 }
 
 /// <summary>
@@ -41,18 +45,18 @@ public interface IBlockCipherServiceFactory
 public class BlockCipherServiceFactory : IBlockCipherServiceFactory
 {
     /// <inheritdoc />
-    public IBlockCipherService CreateEcbBlockCipherService(Func<IBlockCipher> engineFactory)
-        => new BlockCipherService(() => new BufferedBlockCipher(new EcbBlockCipher(engineFactory())));
+    public IBlockCipherService CreateEcbBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize = 4096)
+        => new BlockCipherService(() => new BufferedBlockCipher(new EcbBlockCipher(engineFactory())), bufferSize);
 
     /// <inheritdoc />
-    public IBlockCipherService CreateCbcBlockCipherService(Func<IBlockCipher> engineFactory)
-        => new BlockCipherService(() => new BufferedBlockCipher(new CbcBlockCipher(engineFactory())));
+    public IBlockCipherService CreateCbcBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize = 4096)
+        => new BlockCipherService(() => new BufferedBlockCipher(new CbcBlockCipher(engineFactory())), bufferSize);
 
     /// <inheritdoc />
-    public IBlockCipherService CreateSicBlockCipherService(Func<IBlockCipher> engineFactory)
-        => new BlockCipherService(() => new BufferedBlockCipher(new SicBlockCipher(engineFactory())));
+    public IBlockCipherService CreateSicBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize = 4096)
+        => new BlockCipherService(() => new BufferedBlockCipher(new SicBlockCipher(engineFactory())), bufferSize);
 
     /// <inheritdoc />
-    public IBlockCipherService CreateCfbBlockCipherService(Func<IBlockCipher> engineFactory, int bitBlockSize = 128)
-        => new BlockCipherService(() => new BufferedBlockCipher(new CfbBlockCipher(engineFactory(), bitBlockSize)));
+    public IBlockCipherService CreateCfbBlockCipherService(Func<IBlockCipher> engineFactory, int bitBlockSize = 128, int bufferSize = 4096)
+        => new BlockCipherService(() => new BufferedBlockCipher(new CfbBlockCipher(engineFactory(), bitBlockSize)), bufferSize);
 }
