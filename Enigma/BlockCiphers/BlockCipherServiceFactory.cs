41e047ddc6ab973d904883df02d463f5aaae8509
Josué Clément <josue.d.clement@gmail.com>
2025-03-27 18:16:03 +0100

    Move IBlockCipherServiceFactory to its own file

--- Enigma/BlockCiphers/BlockCipherServiceFactory.cs
+++ Enigma/BlockCiphers/BlockCipherServiceFactory.cs
@@ -5,41 +5,6 @@ using System;
 namespace Enigma.BlockCiphers;
 
 /// <summary>
-/// Definition for block cipher service factory
-/// </summary>
-public interface IBlockCipherServiceFactory
-{
-    /// <summary>
-    /// Create a block cipher service with Electronic Code Book (ECB) mode
-    /// </summary>
-    /// <param name="engineFactory">Engine factory</param>
-    /// <param name="bufferSize">Buffer size</param>
-    IBlockCipherService CreateEcbBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize);
-    
-    /// <summary>
-    /// Create a block cipher service with Cipher-Block-Chaining (CBC) mode
-    /// </summary>
-    /// <param name="engineFactory">Engine factory</param>
-    /// <param name="bufferSize">Buffer size</param>
-    IBlockCipherService CreateCbcBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize);
-    
-    /// <summary>
-    /// Create a block cipher service with Segmented Integer Counter (SIC) mode
-    /// </summary>
-    /// <param name="engineFactory">Engine factory</param>
-    /// <param name="bufferSize">Buffer size</param>
-    IBlockCipherService CreateSicBlockCipherService(Func<IBlockCipher> engineFactory, int bufferSize);
-    
-    /// <summary>
-    /// Create a block cipher service with Cipher-FeedBack (CFB) mode
-    /// </summary>
-    /// <param name="engineFactory">Engine factory</param>
-    /// <param name="bitBlockSize">Bit block size</param>
-    /// <param name="bufferSize">Buffer size</param>
-    IBlockCipherService CreateCfbBlockCipherService(Func<IBlockCipher> engineFactory, int bitBlockSize, int bufferSize);
-}
-
-/// <summary>
 /// Block cipher service factory
 /// </summary>
 public class BlockCipherServiceFactory : IBlockCipherServiceFactory
