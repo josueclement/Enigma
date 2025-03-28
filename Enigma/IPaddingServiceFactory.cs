53fc0b2d26d4d02a261d5be0910d8394a7c5348e
Josué Clément <josue.d.clement@gmail.com>
2025-03-28 09:38:12 +0100

    Improve comments

--- Enigma/IPaddingServiceFactory.cs
+++ Enigma/IPaddingServiceFactory.cs
@@ -11,22 +11,24 @@ public interface IPaddingServiceFactory
     IPaddingService CreateNoPaddingService();
     
     /// <summary>
-    /// Create a padding service with Pkcs7
+    /// Create a padding service with a padder that adds PKCS7/PKCS5 padding to a block.
     /// </summary>
     IPaddingService CreatePkcs7PaddingService();
     
     /// <summary>
-    /// Create a padding service with Iso7816
+    /// Create a padding service with a padder that adds the padding according to the scheme referenced in ISO 7814-4 - scheme 2 from ISO 9797-1.
+    /// The first byte is 0x80, rest is 0x00
     /// </summary>
     IPaddingService CreateIso7816PaddingService();
     
     /// <summary>
-    /// Create a padding service with Iso10126
+    /// Create a padding service with a padder that adds ISO10126-2 padding to a block.
     /// </summary>
     IPaddingService CreateIso10126PaddingService();
     
     /// <summary>
-    /// Create a padding service with X9.23
+    /// Create a padding service with a padder that adds X9.23 padding to a block - if a SecureRandom is passed in random padding is assumed,
+    /// otherwise padding with zeros is used.
     /// </summary>
     IPaddingService CreateX923PaddingService();
 }
