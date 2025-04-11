620ad5ff9f87f50c8234aa29e75475b06e1f474c
Josué Clément <josue.d.clement@gmail.com>
2025-04-11 23:19:04 +0200

    Add block cipher parameters factory

---
+++ Enigma/BlockCiphers/BlockCipherParametersFactory.cs
@@ -0,0 +1,30 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace Enigma.BlockCiphers;
+
+/// <summary>
+/// Block cipher parameters factory
+/// </summary>
+public class BlockCipherParametersFactory : IBlockCipherParametersFactory
+{
+    /// <inheritdoc />
+    public ICipherParameters CreateEcbParameters(byte[] key)
+        => new KeyParameter(key);
+
+    /// <inheritdoc />
+    public ICipherParameters CreateCbcParameters(byte[] key, byte[] iv)
+        => new ParametersWithIV(new KeyParameter(key), iv);
+
+    /// <inheritdoc />
+    public ICipherParameters CreateSicParameters(byte[] key, byte[] nonce)
+        => new ParametersWithIV(new KeyParameter(key), nonce);
+
+    /// <inheritdoc />
+    public ICipherParameters CreateGcmParameters(byte[] key, byte[] nonce, int macSize = 128)
+        => new AeadParameters(new KeyParameter(key), macSize, nonce);
+
+    /// <inheritdoc />
+    public ICipherParameters CreateGcmParameters(byte[] key, byte[] nonce, byte[] associatedText, int macSize = 128)
+        => new AeadParameters(new KeyParameter(key), macSize, nonce, associatedText);
+}
