e458efaba715a365a0ab8aa10b54dc9875de4869
Josué Clément <josue.d.clement@gmail.com>
2025-04-11 23:54:39 +0200

    Process stream ciphers with CipherStream class

--- Enigma/StreamCiphers/StreamCipherService.cs
+++ Enigma/StreamCiphers/StreamCipherService.cs
@@ -1,9 +1,9 @@
 ﻿using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Crypto;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System;
+using Org.BouncyCastle.Crypto.IO;
 
 namespace Enigma.StreamCiphers;
 
@@ -12,33 +12,15 @@ namespace Enigma.StreamCiphers;
 /// </summary>
 /// <param name="cipherFactory">Cipher factory</param>
 /// <param name="bufferSize">Buffer size</param>
-public class StreamCipherService(Func<IStreamCipher> cipherFactory, int bufferSize = 4096) : IStreamCipherService
+public class StreamCipherService(Func<IBufferedCipher> cipherFactory, int bufferSize = 4096) : IStreamCipherService
 {
-    /// <inheritdoc />
-    public (int keySizeInBytes, int nonceSizeInBytes) GetKeyNonceSize()
-    {
-        var cipher = cipherFactory();
-        var engineName = cipher.AlgorithmName.Split('/').FirstOrDefault();
-
-        if (engineName is null)
-            throw new InvalidOperationException("Algorithm name not found");
-
-        return engineName switch
-        {
-            "ChaCha7539" => (32, 12),
-            "ChaCha20" => (32, 8),
-            "Salsa20" => (32, 8),
-            _ => throw new NotImplementedException($"GetKeyNonceSize not implemented for {engineName}")
-        };
-    }
-    
     /// <inheritdoc />
     public async Task EncryptAsync(Stream input, Stream output, byte[] key, byte[] nonce)
     {
         var cipher = cipherFactory();
         var parameters = new ParametersWithIV(new KeyParameter(key), nonce);
         cipher.Init(true, parameters);
-        await ProcessStreamsAsync(input, output, cipher).ConfigureAwait(false);
+        await EncryptStreamAsync(input, output, cipher).ConfigureAwait(false);
     }
 
     /// <inheritdoc />
@@ -47,19 +29,28 @@ public class StreamCipherService(Func<IStreamCipher> cipherFactory, int bufferSi
         var cipher = cipherFactory();
         var parameters = new ParametersWithIV(new KeyParameter(key), nonce);
         cipher.Init(false, parameters);
-        await ProcessStreamsAsync(input, output, cipher).ConfigureAwait(false);
+        await DecryptStreamAsync(input, output, cipher).ConfigureAwait(false);
     }
-
-    private async Task ProcessStreamsAsync(Stream input, Stream output, IStreamCipher cipher)
-    {
-        var buffer = new byte[bufferSize];
-        var outputBuffer = new byte[bufferSize];
-        int bytesRead;
     
+    private async Task EncryptStreamAsync(Stream input, Stream output, IBufferedCipher cipher)
+    {
+        using var cipherStream = new CipherStream(output, null, cipher);
+        var buffer = new byte[bufferSize];
+        int bytesRead;
         while ((bytesRead = await input.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false)) > 0)
         {
-            cipher.ProcessBytes(buffer, 0, bytesRead, outputBuffer, 0);
-            await output.WriteAsync(outputBuffer, 0, bytesRead).ConfigureAwait(false);
+            await cipherStream.WriteAsync(buffer, 0, bytesRead).ConfigureAwait(false);
         }
+    }
+
+    private async Task DecryptStreamAsync(Stream input, Stream output, IBufferedCipher cipher)
+    {
+        using var cipherStream = new CipherStream(input, cipher, null);
+        var buffer = new byte[bufferSize];
+        int bytesRead;
+        while ((bytesRead = await cipherStream.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false)) > 0)
+        {
+            await output.WriteAsync(buffer, 0, bytesRead).ConfigureAwait(false);
+        } 
     }
 }
