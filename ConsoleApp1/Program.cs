f29862d0e85fce744fe3c135b9fca243f1ba2bfc
Josué Clément <josue.d.clement@gmail.com>
2025-04-12 00:26:29 +0200

    Update README

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,12 +8,12 @@ using Enigma.Padding;
 using Enigma.PublicKey;
 using Enigma.StreamCiphers;
 using Enigma.Utils;
+using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Parameters;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System;
-using Org.BouncyCastle.Crypto.Modes;
 
 namespace ConsoleApp1;
 
@@ -25,33 +25,7 @@ internal static class Program
 
         try
         {
-            // Create a block cipher service for AES/CBC/PKCS7Padding
-            var service = new BlockCipherService("AES/GCM/NoPadding");
 
-            // // Get the key and IV sizes
-            // var (keySizeInBytes, ivSizeInBytes) = service.GetKeyIvSize();
-
-            // Generate random key and iv
-            var key = RandomUtils.GenerateRandomBytes(32);
-            var iv = RandomUtils.GenerateRandomBytes(8);
-            var parameters1 = new AeadParameters(new KeyParameter(key), 128, iv, "Hello world !".GetUtf8Bytes());
-            var parameters2 = new AeadParameters(new KeyParameter(key), 128, iv, "Hello world !".GetUtf8Bytes());
-
-            var data = "This is a secret message !".GetUtf8Bytes();
-
-            // Encrypt
-            using var inputEnc = new MemoryStream(data);
-            using var outputEnc = new MemoryStream();
-            await service.EncryptAsync(inputEnc, outputEnc, parameters1);
-
-            var encrypted = outputEnc.ToArray();
-
-            // Decrypt
-            using var inputDec = new MemoryStream(encrypted);
-            using var outputDec = new MemoryStream();
-            await service.DecryptAsync(inputDec, outputDec, parameters2);
-
-            var decrypted = outputDec.ToArray();
         }
         catch (Exception ex)
         {
