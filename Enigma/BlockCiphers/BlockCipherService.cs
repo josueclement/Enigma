db32f5aab585decbab357208e1e922e2b915bfe8
Josué Clément <josue.d.clement@gmail.com>
2025-04-19 09:55:36 +0200

    Finalize BlockCipherService improvements

--- Enigma/BlockCiphers/BlockCipherService.cs
+++ Enigma/BlockCiphers/BlockCipherService.cs
@@ -1,16 +1,16 @@
 ﻿using Org.BouncyCastle.Crypto.IO;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Security;
+using System.Buffers;
 using System.IO;
 using System.Threading.Tasks;
-using System;
-using System.Buffers;
 using System.Threading;
+using System;
 
 namespace Enigma.BlockCiphers;
 
 /// <summary>
-/// Block cipher service
+/// Provides cryptographic operations for block ciphers
 /// </summary>
 public class BlockCipherService : IBlockCipherService
 {
@@ -19,10 +19,10 @@ public class BlockCipherService : IBlockCipherService
     private readonly ArrayPool<byte> _arrayPool;
 
     /// <summary>
-    /// Constructor for <see cref="BlockCipherService"/>
+    /// Initializes a new instance of the <see cref="BlockCipherService"/> class using a custom cipher factory
     /// </summary>
     /// <param name="cipherFactory">Cipher factory</param>
-    /// <param name="bufferSize">Buffer size</param>
+    /// <param name="bufferSize">Buffer size. Default is 4096 bytes (4kB)</param>
     public BlockCipherService(Func<IBufferedCipher> cipherFactory, int bufferSize = 4096)
     {
         _cipherFactory = cipherFactory;
@@ -31,43 +31,73 @@ public class BlockCipherService : IBlockCipherService
     }
 
     /// <summary>
-    /// Constructor for <see cref="BlockCipherService"/>
+    /// Initializes a new instance of the <see cref="BlockCipherService"/> class using a standard algorithm name
     /// </summary>
-    /// <param name="algorithmName"></param>
-    /// <param name="bufferSize"></param>
+    /// <param name="algorithmName">Algorithm name</param>
+    /// <param name="bufferSize">Buffer size. Default is 4096 bytes (4kB)</param>
+    /// <remarks>
+    /// <para>
+    /// Algorithm name examples:
+    /// <list type="bullet">
+    ///   <item><description>AES/GCM/NoPadding</description></item>
+    ///   <item><description>AES/CBC/PKCS7Padding</description></item>
+    /// </list>
+    /// </para>
+    /// </remarks>
     public BlockCipherService(string algorithmName, int bufferSize = 4096)
         : this(() => CipherUtilities.GetCipher(algorithmName), bufferSize) { }
     
-    
     /// <inheritdoc />
-    public async Task EncryptAsync(Stream input, Stream output, ICipherParameters cipherParameters, CancellationToken cancellationToken = default)
+    public async Task EncryptAsync(
+        Stream input,
+        Stream output,
+        ICipherParameters cipherParameters,
+        IProgress<long>? progress = null,
+        CancellationToken cancellationToken = default)
     {
         var cipher = _cipherFactory();
         cipher.Init(forEncryption: true, cipherParameters);
-        await EncryptStreamAsync(input, output, cipher, cancellationToken).ConfigureAwait(false);
+        await EncryptStreamAsync(input, output, cipher, progress, cancellationToken).ConfigureAwait(false);
     }
 
     /// <inheritdoc />
-    public async Task DecryptAsync(Stream input, Stream output, ICipherParameters cipherParameters, CancellationToken cancellationToken = default)
+    public async Task DecryptAsync(
+        Stream input,
+        Stream output,
+        ICipherParameters cipherParameters,
+        IProgress<long>? progress = null,
+        CancellationToken cancellationToken = default)
     {
         var cipher = _cipherFactory();
         cipher.Init(forEncryption: false, cipherParameters);
-        await DecryptStreamAsync(input, output, cipher, cancellationToken).ConfigureAwait(false);
+        await DecryptStreamAsync(input, output, cipher, progress, cancellationToken).ConfigureAwait(false);
     }
 
-    private async Task EncryptStreamAsync(Stream input, Stream output, IBufferedCipher cipher, CancellationToken cancellationToken)
+    private async Task EncryptStreamAsync(
+        Stream input,
+        Stream output,
+        IBufferedCipher cipher,
+        IProgress<long>? progress = null,
+        CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
         
         using var cipherStream = new CipherStream(output, null, cipher);
         var buffer = _arrayPool.Rent(_bufferSize);
+        
         try
         {
             int bytesRead;
+            long totalBytesProgress = 0;
+            
             while ((bytesRead = await input.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) > 0)
             {
                 cancellationToken.ThrowIfCancellationRequested();
+                
                 await cipherStream.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+
+                totalBytesProgress += bytesRead;
+                progress?.Report(totalBytesProgress);
             }
 
             await cipherStream.FlushAsync(cancellationToken).ConfigureAwait(false);
@@ -79,7 +109,12 @@ public class BlockCipherService : IBlockCipherService
         }
     }
 
-    private async Task DecryptStreamAsync(Stream input, Stream output, IBufferedCipher cipher, CancellationToken cancellationToken)
+    private async Task DecryptStreamAsync(
+        Stream input,
+        Stream output,
+        IBufferedCipher cipher,
+        IProgress<long>? progress = null,
+        CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
         
@@ -89,10 +124,16 @@ public class BlockCipherService : IBlockCipherService
         try
         {
             int bytesRead;
+            long totalBytesProgress = 0;
+            
             while ((bytesRead = await cipherStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) > 0)
             {
                 cancellationToken.ThrowIfCancellationRequested();
+                
                 await output.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+                
+                totalBytesProgress += bytesRead;
+                progress?.Report(totalBytesProgress);
             }
 
             await output.FlushAsync(cancellationToken).ConfigureAwait(false);
--- Enigma/BlockCiphers/IBlockCipherService.cs
+++ Enigma/BlockCiphers/IBlockCipherService.cs
@@ -1,7 +1,8 @@
 using Org.BouncyCastle.Crypto;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Threading;
+using System;
 
 namespace Enigma.BlockCiphers;
 
@@ -16,9 +17,15 @@ public interface IBlockCipherService
     /// <param name="input">The input stream containing the plaintext data</param>
     /// <param name="output">The output stream where encrypted data will be written</param>
     /// <param name="cipherParameters">The parameters to initialize the cipher</param>
-    /// <param name="cancellationToken">Cancellation token to cancel the operation</param>
+    /// <param name="progress">Optional progress reporting mechanism that reports bytes processed.</param>
+    /// <param name="cancellationToken">Optional cancellation token to cancel the operation</param>
     /// <returns>A task representing the asynchronous encryption operation</returns>
-    Task EncryptAsync(Stream input, Stream output, ICipherParameters cipherParameters, CancellationToken cancellationToken = default);
+    Task EncryptAsync(
+        Stream input,
+        Stream output,
+        ICipherParameters cipherParameters,
+        IProgress<long>? progress = null,
+        CancellationToken cancellationToken = default);
     
     /// <summary>
     /// Decrypts data from the input stream to the output stream using the specified cipher parameters
@@ -26,7 +33,13 @@ public interface IBlockCipherService
     /// <param name="input">The input stream containing the encrypted data</param>
     /// <param name="output">The output stream where decrypted data will be written</param>
     /// <param name="cipherParameters">The parameters to initialize the cipher</param>
-    /// <param name="cancellationToken">Cancellation token to cancel the operation</param>
+    /// <param name="progress">Optional progress reporting mechanism that reports bytes processed.</param>
+    /// <param name="cancellationToken">Optional cancellation token to cancel the operation</param>
     /// <returns>A task representing the asynchronous decryption operation</returns>
-    Task DecryptAsync(Stream input, Stream output, ICipherParameters cipherParameters, CancellationToken cancellationToken = default);
+    Task DecryptAsync(
+        Stream input,
+        Stream output,
+        ICipherParameters cipherParameters,
+        IProgress<long>? progress = null,
+        CancellationToken cancellationToken = default);
 }
