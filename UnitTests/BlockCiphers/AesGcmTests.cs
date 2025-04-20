e685b34032c19db375635c401ec7dc5e0658c760
Josué Clément <josue.d.clement@gmail.com>
2025-04-20 09:24:53 +0200

    Add unit tests for AES GCM

---
+++ UnitTests/BlockCiphers/AesGcmTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Enigma.BlockCiphers;
+using Enigma.DataEncoding;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace UnitTests.BlockCiphers;
+
+public class AesGcmTests
+{
+    [Theory]
+    [MemberData(nameof(GetCsvValues))]
+    public async Task CsvEncryptTest(byte[] key, byte[] iv, byte[] data, byte[] encrypted)
+    {
+        var engineFactory = new BlockCipherEngineFactory();
+        var service = new BlockCipherServiceFactory().CreateGcmService(engineFactory.CreateAesEngine);
+        var parameters = new BlockCipherParametersFactory().CreateGcmParameters(key, iv);
+        
+        using var msInput = new MemoryStream(data);
+        using var msOutput = new MemoryStream();
+
+        await service.EncryptAsync(msInput, msOutput, parameters);
+        
+        Assert.Equal(encrypted, msOutput.ToArray());
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetCsvValues))]
+    public async Task CsvDecryptTest(byte[] key, byte[] iv, byte[] data, byte[] encrypted)
+    {
+        var engineFactory = new BlockCipherEngineFactory();
+        var service = new BlockCipherServiceFactory().CreateGcmService(engineFactory.CreateAesEngine);
+        var parameters = new BlockCipherParametersFactory().CreateGcmParameters(key, iv);
+        
+        using var msInput = new MemoryStream(encrypted);
+        using var msOutput = new MemoryStream();
+
+        await service.DecryptAsync(msInput, msOutput, parameters);
+        
+        Assert.Equal(data, msOutput.ToArray()); 
+    }
+    
+    public static IEnumerable<object[]> GetCsvValues()
+    {
+        var hex = new HexService();
+        
+        return File.ReadAllLines(@"BlockCiphers\aes-gcm.csv")
+            .Skip(1)
+            .Select(line =>
+            {
+                var values = line.Split(',');
+                return new object[]
+                {
+                    hex.Decode(values[0]), // key
+                    hex.Decode(values[1]), // iv
+                    hex.Decode(values[2]), // data
+                    hex.Decode(values[3]) // encrypted
+                };
+            });
+    }
+}
